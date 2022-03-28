0c922f23def5dc5b28d0c70e51fd92c2fbf7ccf4
Vinícius Breda <v_i_breda@hotmail.com>
2022-03-27 21:25:04 -0300

    Crating Repo

---
+++ src/main.cpp
@@ -0,0 +1,203 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+#include <LITTLEFS.h>
+//#include "littlefs_helper.h"
+
+BluetoothSerial BT;
+
+#define FORMAT 0 // format on boot if set to 1
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
+
+void createDir(fs::FS &fs, const char * path);
+
+void removeDir(fs::FS &fs, const char * path);
+
+void readFile(fs::FS &fs, const char * path);
+
+void writeFile(fs::FS &fs, const char * path, const char * message);
+
+void appendFile(fs::FS &fs, const char * path, const char * message);
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2);
+
+void deleteFile(fs::FS &fs, const char * path);
+
+
+void setup() {
+  // Setup Serial Monitor
+  Serial.begin(115200);
+  Serial.println();
+
+  // Setup Bluetooth Server
+  if(!BT.begin("Data Transmissor")){
+    Serial.println("BT Device not started");
+  } else {
+    Serial.println("BT Started!");
+  }
+
+  // Setup File System
+  if(!LITTLEFS.begin())
+    Serial.println("LittleFS not Mounted");
+  else
+    Serial.println("LittleFS Mounted!");
+    if(FORMAT){ // Format drive if needed
+      Serial.println("Formatting Drive...");
+      Serial.println((String)"Previously Used Bytes: "+LITTLEFS.usedBytes()+"/"+LITTLEFS.totalBytes());
+      LITTLEFS.format();
+      Serial.println((String)"After formatting: "+LITTLEFS.usedBytes()+"/"+LITTLEFS.totalBytes());
+    }
+    createDir(LITTLEFS, "/storage"); // Create directory for file to be stored
+}
+
+
+void loop() {
+  if(BT.available()){
+    Serial.println("Writing to File loremimpsum.txt...");
+    writeFile(LITTLEFS, "/storage/loremipsum.txt", BT.read());
+    Serial.println("==================== F I L E   C O N T E N T ====================");
+    readFile(LITTLEFS, "/storage/loremipsum.txt");
+
+
+  }
+
+
+}
+
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println(" - not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("- failed to open file for reading");
+        return;
+    }
+
+    Serial.println("- read from file:");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("- failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- message appended");
+    } else {
+        Serial.println("- append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\r\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("- file renamed");
+    } else {
+        Serial.println("- rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+
+// #include <Arduino.h>
+// #include <BluetoothSerial.h>
+// #include <LITTLEFS.h>
+
+// BluetoothSerial SerialBT;
+
+// void setup() {
+//   Serial.begin(115200);
+//   SerialBT.begin("ESP32test"); //Bluetooth device name
+//   Serial.println("The device started, now you can pair it with bluetooth!");
+// }
+
+// void loop() {
+//   if (Serial.available()) {
+//     SerialBT.write(Serial.read());
+//   }
+//   if (SerialBT.available()) {
+//     Serial.write(SerialBT.read());
+//   }
+//   delay(20);
+// }
